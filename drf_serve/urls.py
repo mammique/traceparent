6182fa526f21a1e56b63ad1d1e848e0bfd7bbdc0
Camille Harang <mammique@yooook.net>
2013-04-27 03:00:09 +0200

    drf_serve bucket users_ro update

--- drf_serve/urls.py
+++ drf_serve/urls.py
@@ -5,9 +5,10 @@ from rest_framework.compat import patterns, url
 from rest_framework.views import APIView
 
 from traceparent.utils import ordered_dict
+from traceparent.views import M2MAddView, M2MRemoveView
 
 from .views import bucket_content_nginx #, BucketUpdateView
-
+from .models import Bucket
 
 #class ServeView(APIView):
 #
@@ -36,19 +37,54 @@ from .views import bucket_content_nginx #, BucketUpdateView
         #return Response(data)
 
 
+# FIXME: use RetrieveAPIView?
+class BucketUpdateUsersRoView(APIView):
+
+    def get(self, request, *args, **kwargs):
+
+        if not args: args = (kwargs['user_pk'], kwargs['bucket_slug'],)
+
+        data = ordered_dict(
+            {
+             'add':    reverse('drf_serve_bucket_update_users_ro_add', args,
+                               request=self.request),
+             'remove': reverse('drf_serve_bucket_update_users_ro_remove', args,
+                               request=self.request),
+            }
+        )
+
+        return Response(data)
+
+
+class BucketM2MAddView(M2MAddView):
+
+    def get_object(self, *args, **kwargs):
+
+        # Return object or first queryset entry.
+        return args[0] if isinstance(args[0], Bucket) else args[0][0]
+
+
+class BucketM2MRemoveView(M2MRemoveView):
+
+    def get_object(self, *args, **kwargs):
+
+        # Return object or first queryset entry.
+        return args[0] if isinstance(args[0], Bucket) else args[0][0]
+
+
 urlpatterns = patterns('django.contrib.auth.views',
 
     #url(r'^$', ServeView.as_view(), name=''),
 
     url(r'^bucket/(?P<user_pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/(?P<bucket_slug>[\w]+)/content/(?P<content_path>.*)$',
         bucket_content_nginx, name='drf_serve_bucket_content'),
-    #url(r'^bucket/(?P<user_pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/(?P<bucket_slug>[\w]+)/update/users_ro/$',
-        #ScopeUpdateQuantitiesView.as_view(), name='drf_serve_bucket_update_users_ro'),
-    #url(r'^bucket/(?P<user_pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/(?P<bucket_slug>[\w]+)/update/users_ro/add/$',
-        #M2MAddView.as_view(model=Scope), {'m2m_field': 'users_ro'},
-        #name='drf_serve_bucket_update_users_ro_add'),
-    #url(r'^bucket/(?P<user_pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/(?P<bucket_slug>[\w]+)/update/users_ro/remove/$',
-        #M2MRemoveView.as_view(model=Scope), {'m2m_field': 'users_ro'},
-        #name='drf_serve_bucket_update_users_ro_remove'),
+    url(r'^bucket/(?P<user_pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/(?P<bucket_slug>[\w]+)/update/users_ro/$',
+        BucketUpdateUsersRoView.as_view(), name='drf_serve_bucket_update_users_ro'),
+    url(r'^bucket/(?P<user_pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/(?P<bucket_slug>[\w]+)/update/users_ro/add/$',
+        BucketM2MAddView.as_view(model=Bucket), {'m2m_field': 'users_ro'},
+        name='drf_serve_bucket_update_users_ro_add'),
+    url(r'^bucket/(?P<user_pk>[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})/(?P<bucket_slug>[\w]+)/update/users_ro/remove/$',
+        BucketM2MRemoveView.as_view(model=Bucket), {'m2m_field': 'users_ro'},
+        name='drf_serve_bucket_update_users_ro_remove'),
 
 )
